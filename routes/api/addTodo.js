15058a138e43242ebccde36206ea9ab9ea0016ee
reapedjuggler <tomarvibhav55@gmail.com>
2020-12-29 17:15:49 +0530

    Inital commit
    Signed-off-by: reapedjuggler <tomarvibhav55@gmail.com>

---
+++ routes/api/addTodo.js
@@ -0,0 +1,11 @@
+// const route = require('express').Router();
+// const mongod = require('../../db.js');
+
+// route.post('/', (req, res, next) => {
+//     console.log(req.body);
+//     res.render("index.ejs");
+// });
+
+// exports = module.exports = {
+//     route,
+// }
